677b868272743c5c65c8ebbcfceedd52a6ae77aa
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-09-30 00:08:05 +0530

    button

--- src/components/shopCard.tsx
+++ src/components/shopCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Box, Button, Link } from "@mui/material"
+import { Card, CardContent, Box, Button, Link, Typography } from "@mui/material"
 import Colors from "../config/colors"
 
 type Props = {
@@ -17,47 +17,47 @@ const ShopCard = (props: Props) =>{
                     window.open(urlToOpen, '_blank')
                 }}
                 sx={{
-                    width: '250px',
-                    height: '450px',
+                    width: '200px',
+                    height: '350px',
                     cursor: 'pointer',
                 }}
             >
                 <CardContent>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        gap: '20px'
-                    }}
-                >
-                    <img 
-                        src={image}
-                        alt="image"
-                        width='250px' 
-                        height='250px'
-                    />
-
-                <Box textAlign={'center'}  style={{ fontWeight: 'bold' }}>
-                    {title}
-                </Box>
-                </Box>
-                <div style={{ position: 'relative' }}>
-                    <Button 
-                        onClick={(e)=>{
-                            e.stopPropagation()
-                            const urlToOpen = new URL(link)
-                            window.open(urlToOpen, '_blank')
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            gap: '20px'
                         }}
-                        style={{ position: 'absolute', bottom: '-110px' }} 
-                        fullWidth 
-                        size='large' 
-                        variant='outlined'
-                        sx={{ backgroundColor: '#FFDE00', color: '#000000' }}
                     >
-                        Buy
-                    </Button>
-                </div>
+                        <img 
+                            src={image}
+                            alt="image"
+                            width='150px' 
+                            height='130px'
+                        />
+                        <Box textAlign={'center'}  style={{ fontWeight: 'bold' }}>
+                            {title}
+                        </Box>
+                    </Box>
+                    <Typography variant="body2">From Amazon</Typography>
+                    <div style={{ position: 'relative' }}>
+                        <Button 
+                            onClick={(e)=>{
+                                e.stopPropagation()
+                                const urlToOpen = new URL(link)
+                                window.open(urlToOpen, '_blank')
+                            }}
+                            style={{ position: 'absolute',  bottom: 0 }} 
+                            fullWidth 
+                            size='large' 
+                            variant='contained'
+                            sx={{ backgroundColor: '#FFDE00', color: '#000000' }}
+                        >
+                            Buy
+                        </Button>
+                    </div>
                 </CardContent>
             </Card>
         </>
