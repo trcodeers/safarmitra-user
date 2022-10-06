6ce38f3a3c3f1139f2c496db147c6bf79ffb1573
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-10-06 17:49:40 +0530

    props

--- src/components/share/titlebarImageList.tsx
+++ src/components/share/titlebarImageList.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
 import Image from 'next/image'
 import { Box, Typography } from '@mui/material';
-import { relative } from 'path';
 import Colors from '../../config/colors';
 
-export default function TitlebarImageList() {
-  
+type Props = {
+  data: Array<any>
+}
+export default function TitlebarImageList(props: Props) {
+    const { data } = props
   return (
    <>
       <Box
@@ -23,7 +19,7 @@ export default function TitlebarImageList() {
           flexWrap: 'wrap'
         }}
       >
-        {itemData.map((item: any) => (
+        {data.map((item: any) => (
           <Box
             sx={{
               display: 'flex',
@@ -61,49 +57,3 @@ export default function TitlebarImageList() {
    );
 }
 
-const itemData = [
-  {
-    img: '/placeImages/title.jpg',
-    title: 'ShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimla',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Shimla',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Mussorie',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Manali',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Varanasi',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
--- src/pages/[name].tsx
+++ src/pages/[name].tsx
@@ -65,10 +65,8 @@ const Index: NextPage = () => {
             <Box
                 sx={{
                     display: 'flex', 
-                    // justifyContent: 'center', 
                     flexDirection: 'column',
                     gap: '30px',
-                    // alignItems: 'center',
                     padding: { xs: '2%', sm: '2%' }
                 }}
             >
@@ -99,7 +97,9 @@ const Index: NextPage = () => {
                 }
 
             <div>
-                <TitlebarImageList/>
+                <TitlebarImageList
+                    data={itemData}
+                />
 
             </div>
 
@@ -108,5 +108,53 @@ const Index: NextPage = () => {
     )
 
 }
+
+
+const itemData = [
+    {
+      img: '/placeImages/title.jpg',
+      title: 'ShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimlaShimla',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Shimla',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Mussorie',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Manali',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Varanasi',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+      title: 'Tomato basil',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+      title: 'Sea star',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+      title: 'Bike',
+    },
+  ];
 
 export default Index;
