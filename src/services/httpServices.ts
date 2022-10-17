f2a8fc8fb16e517e7b6b4e91e5a9a9733a6cf9b6
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-10-17 14:59:33 +0530

    change bvase url

--- src/services/httpServices.ts
+++ src/services/httpServices.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setup } from 'axios-cache-adapter'
-const baseURL = 'https://safarmitra-api.herokuapp.com'
+const baseURL = 'https://api-safarmitra.herokuapp.com'
 
 const api = setup({
     baseURL: baseURL,
